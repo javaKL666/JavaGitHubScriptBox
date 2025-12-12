03d980062399a8e1b066d6f50bb51441532d8675
javaKL666 <xb6666661979@outlook.com>
2025-12-12 19:05:23 +0800

    Update URL for WorkScript loading

--- WorkScript.lua
+++ WorkScript.lua
@@ -1,1 +1,1 @@
-getgenv().WorkScript = "工脚本" 	loadstring(game:HttpGet("https://raw.githubusercontent.com/javaKL666/JavaGitHubScriptBox/refs/heads/main/Roblox-Scriptbox-WorkScript.lua"))()
+getgenv().WorkScript = "工脚本" 	loadstring(game:HttpGet("https://raw.githubusercontent.com/javaKL666/JavaScriptBox/refs/heads/main/Roblox-Scriptbox-WorkScript.lua"))()
